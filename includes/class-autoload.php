72b75e055610c0b8858801717d6b2a7d9c855882
davidp <dpotter05@gmail.com>
2022-12-30 21:53:27 +0000

    Improve autoloader

--- includes/class-autoload.php
+++ includes/class-autoload.php
@@ -5,7 +5,8 @@ namespace getdown\autoload;
 class Autoload {
     public function __construct() {
         spl_autoload_register(function ( $class_name_with_namespace ) {
-            if ( $class_name_with_namespace !== 'WP_Site_Health' ) {
+            $namespace_slug = 'getdown\\';
+            if ( str_contains( $class_name_with_namespace, $namespace_slug ) ) {
                 $pos = strrpos( $class_name_with_namespace, '\\' ) + 1;
                 $file_name = strtolower( substr( $class_name_with_namespace, $pos ) );
                 $file_name = 'class-' . str_replace( '_', "-", $file_name ) . '.php';
